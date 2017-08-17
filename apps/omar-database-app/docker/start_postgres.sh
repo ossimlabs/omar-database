067049e22c093a8f24cc39dfc04292975d70d3af
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2017-08-17 02:22:13 -0400

    revert

--- apps/omar-database-app/docker/start_postgres.sh
+++ apps/omar-database-app/docker/start_postgres.sh
@@ -11,44 +11,40 @@ __create_user() {
   usermod -G wheel,adm postgres
 
   # Check to see if we have pre-defined credentials to use
-#if [ -n "${DB_USER}" ]; then
-##  if [ -z "${DB_PASS}" ]; then
-##    echo ""
-##    echo "WARNING: "
-##    echo "No password specified for \"${DB_USER}\". Generating one"
-##    echo ""
-##    DB_PASS=$(pwgen -c -n -1 12)
-##    echo "Password for \"${DB_USER}\" created as: \"${DB_PASS}\""
-##  fi
-#    echo "Creating user \"${DB_USER}\"..."
-#    echo "CREATE ROLE ${DB_USER} with CREATEROLE login superuser;" |
-#      sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
-#       -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
-#
-#fi
-
-#if [ -n "${DB_NAME}" ]; then
-#  echo "Creating database \"${DB_NAME}\"..."
-#  echo "CREATE DATABASE ${DB_NAME};" | \
-#    sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
-#     -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
-#
-#  if [ -n "${DB_USER}" ]; then
-#    echo "Granting access to database \"${DB_NAME}\" for user \"${DB_USER}\"..."
-#    echo "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} to ${DB_USER};" |
-#      sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
-#      -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
+if [ -n "${DB_USER}" ]; then
+#  if [ -z "${DB_PASS}" ]; then
+#    echo ""
+#    echo "WARNING: "
+#    echo "No password specified for \"${DB_USER}\". Generating one"
+#    echo ""
+#    DB_PASS=$(pwgen -c -n -1 12)
+#    echo "Password for \"${DB_USER}\" created as: \"${DB_PASS}\""
 #  fi
-#
-#  if [ -n "${SQL_SCRIPT}" ]; then
-#    echo "Running SQL script \"${SQL_SCRIPT}\" on Database \"${DB_NAME}\" for user \"${DB_USER}\"..."
-#    sudo -u postgres -H psql -d ${DB_NAME} -f ${SQL_SCRIPT}
-#  fi
-#fi
-    echo "Creating o2_omar_db as user postgres"
-    su - postgres -c createdb o2_omar_db
-    su - postgres -c psql -d o2_omar_db -f /var/lib/pgsql/9.4/data/omardb.sql
+    echo "Creating user \"${DB_USER}\"..."
+    echo "CREATE ROLE ${DB_USER} with CREATEROLE login superuser;" |
+      sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
+       -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
 
+fi
+
+if [ -n "${DB_NAME}" ]; then
+  echo "Creating database \"${DB_NAME}\"..."
+  echo "CREATE DATABASE ${DB_NAME};" | \
+    sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
+     -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
+
+  if [ -n "${DB_USER}" ]; then
+    echo "Granting access to database \"${DB_NAME}\" for user \"${DB_USER}\"..."
+    echo "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} to ${DB_USER};" |
+      sudo -u postgres -H /usr/pgsql-9.4/bin/postgres --single \
+      -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
+  fi
+
+  if [ -n "${SQL_SCRIPT}" ]; then
+    echo "Running SQL script \"${SQL_SCRIPT}\" on Database \"${DB_NAME}\" for user \"${DB_USER}\"..."
+    sudo -u postgres -H psql -U ${DB_USER} -d ${DB_NAME} -f ${SQL_SCRIPT}
+  fi
+fi
 }
 
 
